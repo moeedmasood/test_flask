f00065d5ca7eeabd9d4fbf6f931682512af46df4
Moeed Masood <moeedmasood@hotmail.com>
2023-09-24 16:59:22 +1000

    add routing and data exchange

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 # public variable with __
 
@@ -7,8 +7,27 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello From Flask App"
+    name = input("User please tell me your name: ")
+    return render_template(
+        "home.html", data={"name": name}
+    )  # always send data in dictionary variable
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/results", methods=["GET", "POST"])
+def results():
+    if request.method == "POST":
+        num1 = request.form["num1"]
+        num2 = request.form["num2"]
+        result = int(num1) + int(num2)
+        return render_template("results.html", answer={"result": result})
+    else:
+        return render_template("results.html", answer={"result": "None"})
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
